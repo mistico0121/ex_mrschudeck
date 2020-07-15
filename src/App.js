621bb2cb387a672749988c3533624e64c52e65b6
mistico0121 <mrschudeck@uc.cl>
2020-07-14 22:12:01 -0400

    ayudaaaa

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import BoatSelect from './BoatSelect';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+			<h1>GO CHIMP</h1>
+	      
+			<BoatSelect/>
+      
+		</div>
+  );
+}
+
+export default App;
