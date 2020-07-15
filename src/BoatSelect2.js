d312e8baa5bc53cffd402629ba8e7d643a194139
mistico0121 <mrschudeck@uc.cl>
2020-07-14 23:43:44 -0400

    avances

--- src/BoatSelect2.js
+++ src/BoatSelect2.js
@@ -4,39 +4,39 @@ class BoatSelect extends Component{
 	//ALL INFO ON APP, AND HOW IT CHANGES OVER TIME :DDD
 	state = {
 		boats: ['F1', 'F2', 'F3', 'F4', 'C1', 'C2', 'C3', 'D1', 'D2', 'P1'],
-		currentBoatSelect: '',
-		matriz: [],
-		readyBoats:[]
-	};
+		currentBoatSelect: 'F',
+		matriz: []
+	}
 
 	setBoat = (boat) => {
-		this.setState((prevState)=>({
-			currentBoatSelect: boat,
-			readyBoats: [...prevState.readyBoats, boat]
-		
-		}),()=>{
-			//current value is updated here
-			if (this.state.readyboats.length ==2){
-				
-			}
-		});
+		this.setState(prevState)=>({
+			currentBoatSelect: boat
+		})
+
 	}
-	
+
 	//WHAT SHOULD THE USER SEE WHEN THE STATE CHANGES WOW
 	render(){
+
+		for (let i = 0; i<10; i++){
+			let row = [];
+			for (let j = 0; j<10; j++){
+				row.push(0);
+			}
+			this.state.matriz.push(row);
+		}	
 
 		return (
 			<div>
 				<h1>Bote a poner {this.state.currentBoatSelect}</h1>
-				<h1>Botes puestos {this.state.readyBoats}</h1>
+				
 				{
 					//RETORNA UN NUEVO ARRAY QUE LE PASA UNA FUNCION  A CADA ELEMENTO DEL ARRAY PREVIO
 					this.state.boats.map(boat =>(
 						
 						<img 
 							key = {boat} 
-							onClick = {()=> this.setBoat(boat)
-								}
+							onClick = {()=> this.setBoat(boat)}
 							className = 'smallImg' 
 							src ={`./botes/${boat[0]}.png`} />
 						))
